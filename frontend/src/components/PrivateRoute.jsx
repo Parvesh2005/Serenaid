6e95e216513ce6cd4bd4366a1a74115da73037b9
Parvesh2005 <parveshsgulati@gmail.com>
2025-06-27 12:05:53 +0530

    Protect Routes

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,14 +1,12 @@
 import { Navigate } from "react-router";
 import { UserAuth } from "../context/AuthContex"
 
-const PrivateRoute = () => {
-    const { session } = ({ children }) => {
-        const { session } = UserAuth;
+const PrivateRoute = ({children}) => {
+        const { session } = UserAuth();
 
         return <>
             { session ? <>{children}</> : <Navigate to = "/signup" /> }
         </>
-    }
 }
 
 export default PrivateRoute;
