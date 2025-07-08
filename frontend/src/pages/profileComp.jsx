3e7cc4d25f58d83818fdde69482d07f0ca2329d4
Parvesh2005 <parveshsgulati@gmail.com>
2025-07-08 17:12:38 +0530

    2

--- frontend/src/pages/profileComp.jsx
+++ frontend/src/pages/profileComp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { UserAuth } from "../context/AuthContex";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 
 const DynamicUserForm = () => {
   const { session } = UserAuth();
